93066de4959e76ae27c85c83488defedeca266b7
Isaiah <medinaj22@mail.wlu.edu>
2020-10-24 17:19:11 -0400

    This is updated page outline for our deadline on Tuesday. Most recent: 10/24/20

--- pageoutline?.R
+++ pageoutline?.R
@@ -1,5 +1,5 @@
 
-
+library(markdown)
 library(shiny)
 library(shinyWidgets)
 
@@ -12,35 +12,73 @@ setBackgroundColor (
     direction = "bottom"
   ),
   
-  
-  titlePanel("Income & Health Inequality in America"),
-  sidebarLayout(
-    sidebarPanel(
-      h2("Quick Acknowledgment"),
-      p("We would like to thank Dr. Gregg Whitworth for his support in the development of this website as well
-        as Dr. Jonathan Eastwood for his advice on the outline of this project."),
-      br()
-    ),
-    mainPanel(
-      h1("The Why Behind the Project"),
-      p("The year 2020 has certaintly not been an easy one for the United States overall.", 
-        ("It has also shown many issues that as a nation we have tried to sweep under the rug,"), 
-        "such as income disparity and health outcomes of certain communities."),
-      br(),
-      p("For these reasons we decided to look at income disparity between whites and blacks over time in the U.S.
+
+ui <- navbarPage("Inequality Navigation Bar",
+                 tabPanel("Home", 
+                          titlePanel("Income & Health Inequality in America"),
+                          mainPanel(
+                            h1("The Why Behind the Project"),
+                            p("The year 2020 has certaintly not been an easy one for the United States overall.", 
+                              ("It has also shown many issues that as a nation we have tried to sweep under the rug,"), 
+                              "such as income disparity and health outcomes of certain communities."),
+                            br(),
+                            p("For these reasons we decided to look at income disparity between whites and blacks over time in the U.S.
         as well as life expectancy in U.S. counties as of 2017. If this topic interests you, further reasearch can be done here.",
-        a("Pew Research Center.", 
-          href = "https://www.pewsocialtrends.org/2016/06/27/1-demographic-trends-and-economic-well-being/")),
-      br(),
-      h2("Few of the Things to be Shown"),
-      p("- Life Expectancy between whites and blacks in the United States over time."),
-      p("- Economic disparity over time between whites and blacks in the United States." 
-        ),
-      p("- Life Expectancy by counties in the United States."),
-      p("- Data clean up for future researchers to be able to use readily.")
+                              a("Pew Research Center.", 
+                                href = "https://www.pewsocialtrends.org/2016/06/27/1-demographic-trends-and-economic-well-being/")),
+                            br(),
+                            h1("Few of the Things to be Shown"),
+                            p("- Life Expectancy between whites and blacks in the United States over time."),
+                            p("- Economic disparity over time between whites and blacks in the United States." 
+                            ),
+                            p("- Life Expectancy by counties in the United States."),
+                            p("- Data clean up for future researchers to be able to use readily.")
+                          )
+    ),
+    tabPanel("Life Expectancy Disparity by Race"),
+    tabPanel("Life Expectancy by Counties"),
+    tabPanel("Income Disparity by Race"),
+    tabPanel("Data & Acknowlegments",
+             h2("Acknowledgments"),
+             p("We would first like to thank Dr. Gregg Whitworth for assisting us with this project. He helped
+               with everything from the brainstorming process to helping us create the code for visualizing the data we found.
+               We would also like to thank Dr. Jonathan Eastwood as he provided advice on the brainstorming and execution aspects of this
+               project which were helpful considering this topic is his speciality."),
+             br(),
+             p("We hope that these results find their way to people who can continue carrying out the analysis that we started
+               as a class project. Future projects could show the inequality between whites and other races/ethnicities in America,
+               such as Hispanics and Asians,  or compare other areas of inequality."),
+             br(),
+             p("The data for this project was not as easy to collect as we originally thought. The data was also not as clean
+               as it could have been, expecially datasets from the CDC. As a result, we decided to clean the data as best as possible
+               and post it here so that future studies may be able to have the data readily available."),
+             h4("Download County-level Poverty Data"),
+             downloadBttn(
+               outputId = "downloadData",
+               style = "pill",
+               color = "primary",
+               size = "sm"
+             ),
+             h4("Download Life Expectancy by Race Data"),
+             downloadBttn(
+               outputId = "downloadData",
+               style = "pill",
+               color = "primary",
+               size = "sm"
+             ),
+             h4("Download Income Inequality by Race Data"),
+             downloadBttn(
+               outputId = "downloadData",
+               style = "pill",
+               color = "primary",
+               size = "sm"
+             )
     )
   )
 )
+  
+
+
 
 # Define server logic ----
 server <- function(input, output) {
