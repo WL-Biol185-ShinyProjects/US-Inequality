6b5bd1886e3c536028f08655a63f37424cbeae37
Isaiah <medinaj22@mail.wlu.edu>
2020-11-06 11:02:06 -0500

    So Dr. W's code from before stopped working. Don't know what to do and at this point I am sicking of looking for error i don't know where to search for. So i made these as a backup plan.

---
+++ mapAttemptparce/notHelping4.R
@@ -0,0 +1,4 @@
+geo <- readOGR("counties.json")
+leaflet(geo) %>%
+  setView(-96, 37.8, 4) %>% 
+  addPolygons(fillColor = ~pal(health_ineq_online_table_11$le_agg_q4_F), weight = 2, opacity = "white", dashArray = "3", fillOpacity = 0.7)
