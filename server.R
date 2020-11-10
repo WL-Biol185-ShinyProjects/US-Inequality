88cd5f45a2b726d50b89ef0b53227c55d995e8dc
Bella-fp <frazier-pool22@mail.wlu.edu>
2020-11-10 13:35:40 -0500

    changed locations of ui.r and server.r trial

--- server.R
+++ server.R
@@ -1,84 +1,69 @@
 library(shiny)
-library(shinyWidgets)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
 function(input, output) {
   
-  #code for LE by county data; this will be leaflets and drop down selector
-  
-  output$LECountyandIncomePercentile<- renderLeaflet(
-    {
-    
-    if (input$CountyChoices == "Q1") {
-      # code to make map #1
-      geo <- readOGR("counties.json")
-      
-      healthTable1 <- read_csv("health_ineq_online_table_11.csv")
-      
-      healthData1 <- left_join(geo@data, healthTable1, by = c("COUNTY" = "county_name"))
-      geo@data <- healthData1
-      
-      bins <- c(77, 79, 81, 83, 85, 87, 89, Inf)
-      pal <- colorBin("YlOrRd", domain = healthTable1, bins = bins)
-      
-      leaflet(geo) %>%
-        setView(-96, 37.8, 4) %>% 
-        addPolygons(fillColor = ~pal(health_ineq_online_table_11$le_agg_q1_M), weight = 2, opacity = "white", dashArray = "3", fillOpacity = 0.7) %>%
-        addLegend("bottomright", pal = pal, values = ~bins,
-                  title = "LE by County",
-                  labFormat = labelFormat(prefix = "Years: "),
-                  opacity = 1
-        )
-
-    } 
-      else if (input$CountyChoices == "Q2") {
-      #code to make map 2
-      geo <- readOGR("counties.json")
-      
-      healthTable1 <- read_csv("health_ineq_online_table_11.csv")
-      
-      healthData1 <- left_join(geo@data, healthTable1, by = c("COUNTY" = "county_name"))
-      geo@data <- healthData1
-      
-      bins <- c(77, 79, 81, 83, 85, 87, 89, Inf)
-      pal <- colorBin("YlOrRd", domain = healthTable1, bins = bins)
-      
-      leaflet(geo) %>%
-        setView(-96, 37.8, 4) %>% 
-        addPolygons(fillColor = ~pal(health_ineq_online_table_11$le_agg_q2_M), weight = 2, opacity = "white", dashArray = "3", fillOpacity = 0.7) %>%
-        addLegend("bottomright", pal = pal, values = ~bins,
-                  title = "LE by County",
-                  labFormat = labelFormat(prefix = "Years: "),
-                  opacity = 1
-        )
-      
-    } 
-      else {
-      #code to make map 3
-      geo <- readOGR("counties.json")
-      
-      healthTable1 <- read_csv("health_ineq_online_table_11.csv")
-      
-      healthData1 <- left_join(geo@data, healthTable1, by = c("COUNTY" = "county_name"))
-      geo@data <- healthData1
-      
-      bins <- c(77, 79, 81, 83, 85, 87, 89, Inf)
-      pal <- colorBin("YlOrRd", domain = healthTable1, bins = bins)
-      
-      
-      leaflet(geo) %>%
-        setView(-96, 37.8, 4) %>% 
-        addPolygons(fillColor = ~pal(health_ineq_online_table_11$le_agg_q3_M), weight = 2, opacity = "white", dashArray = "3", fillOpacity = 0.7) %>%
-        addLegend("bottomright", pal = pal, values = ~bins,
-                  title = "LE by County",
-                  labFormat = labelFormat(prefix = "Years: "),
-                  opacity = 1
-        )
-    }
-    
-  }
-  )
-  
   #code for timelapse of income inequality among blacks and whites
+  blackonlyincome <- read_excel("blackonlyincome.xlsx")
+  whiteonlyincome <- read_excel("whiteonlyincome.xlsx")
+  combined_income <- read_excel("combined income.xlsx")
   
+  output$incomeplot <- renderPlot({
+    combined_income%>%
+      filter(Year==input$Year)%>%
+      ggplot(aes(Race, `Mean Income 2019`, fill = Race)) + 
+      geom_bar(stat = "identity") +
+      ggtitle("Income Inequality  Over Time") +
+      xlab("Race") + ylab("Average Yearly Income") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    
+    
+  })
+  
+  output$blackincome <- renderPlot({
+    blackonlyincome%>%
+      ggplot(aes(Year, `Mean Income 2019`)) +
+      geom_point() +
+      ggtitle("Black Income") +
+      xlab("Year") + ylab("Income") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
+  })
+  
+  output$whiteincome <- renderPlot({
+    whiteonlyincome %>%
+      ggplot(aes(Year, `Mean Income 2019`)) +
+      geom_point() +
+      ggtitle("White Income") +
+      xlab("Year") + ylab("Income") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
+  })
+  
+  
+}
+
+  
+  #code for life expectancy
+  NCHS_Death_rates_and_life_expectancy_at_birth <- read.csv("NCHS_Death_rates_and_life_expectancy_at_birth.csv")
+  Year<- NCHS_Death_rates_and_life_expectancy_at_birth
+  
+  output$LifeExpectancy<- renderPlot({
+    NCHS_Death_rates_and_life_expectancy_at_birth %>%
+      filter(Race == input$Race, Sex == input$Sex) %>%
+      ggplot(aes(Race, `Average Life Expectancy (Years)`, colour= Sex))+
+      geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    
+  })
+  
+  
+  output$LifeExpectancyInfo <-renderDataTable({
+    clickEvent<- input$LifeExpectancyPlotClick
+    NCHS_Death_rates_and_life_expectancy_at_birth %>%
+      nearPoints(clickEvent)
+  })
+  
+
   # County Life Expectancy Data
   CountyLevelLE <- health_ineq_online_table_11
   
@@ -129,4 +114,3 @@ function(input, output) {
     }
   )
   
-}
