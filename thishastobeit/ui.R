a2aa7facec4b902ce951f7f1103f4333b89804fa
Bella-fp <frazier-pool22@mail.wlu.edu>
2020-11-10 13:27:21 -0500

    Something keeps crashing on my end, will have to fix.

--- thishastobeit/ui.R
+++ thishastobeit/ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(tidyverse)
-library(ggplot2)
+library(shinyWidgets)
 fluidPage(
   setBackgroundColor (
     color = c("#F8F8FF", "#CFEEFA"),
@@ -38,19 +38,20 @@ fluidPage(
                                  label = "Year",
                                  min = 1900,
                                  max = 2019,
+                                 value = 1950,
                                  animate = animationOptions(interval = 1000, loop = FALSE)
                      ),
                      
                      selectInput(inputId = "Sex",
                                  label = "Select Sex",
                                  multiple = TRUE,
-                                 choices = unique(NCHS_._Death_rates_and_life_expectancy_at_birth$Sex),
+                                 choices = unique(NCHS_Death_rates_and_life_expectancy_at_birth$Sex),
                                  selected = "Both Sexes"),
                      
                      selectInput(inputId = "Race",
                                  label = "Select Race",
                                  multiple = TRUE,
-                                 choices = unique(NCHS_._Death_rates_and_life_expectancy_at_birth$Race),
+                                 choices = unique(NCHS_Death_rates_and_life_expectancy_at_birth$Race),
                                  selected = "All Races")
                      
                      
